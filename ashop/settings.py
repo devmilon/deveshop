09a36b7d2b35ca34fda4e6d7142bbd964a91bd61
devmilon <jahidhasanmilon815@gmail.com>
2026-03-30 16:27:42 -0400

    Update settings.py

--- ashop/settings.py
+++ ashop/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ----------------------
 SECRET_KEY = config('SECRET_KEY', default='unsafe-secret-key-for-dev')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*')
 
 
 TEMPLATE_DIR = os.path.join(BASE_DIR , 'template')
